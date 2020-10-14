ef8c5fa946591ee1e02e9763ce2a83d7d16bf31e
Shiftgithub <sajibadhikery@gmail.com>
2020-10-14 19:14:45 +0600

    Dart Start

---
+++ trial.dart
@@ -0,0 +1,13 @@
+void main(){
+  
+  List ripn = ['apple', "banna", 33, true, 's'];
+
+  print(ripn[1]);
+
+  ripn.add("Robin <3 Sokhina");
+  print(ripn);
+
+  ripn.remove('s');
+  print(ripn);
+
+}
