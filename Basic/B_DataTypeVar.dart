69835946b0f1b2cef94a02f7d8f23326993f46b2
Shiftgithub <60180521+Shiftgithub@users.noreply.github.com>
2020-10-15 21:26:37 +0600

    data_type

---
+++ Basic/B_DataTypeVar.dart
@@ -0,0 +1,42 @@
+void main() {
+  //Declare variable x
+  var x;
+
+  // Define Integer
+  x = 12;
+  print(x);
+
+  // Define Double
+  x = 12.2222;
+  print(x);
+
+  // Define String
+  x = "hey Dart!";
+  print(x);
+
+  // Define bool
+  x = true;
+  print(x);
+
+
+  /**
+   * Data Types
+   */
+  
+
+  // Define Integer
+  int numi = 12;
+  print(numi);
+
+  // Define Double
+  double numd = 12.2222;
+  print(numd);
+
+  // Define String
+  String str = "hey Dart!";
+  print(str);
+
+  // Define bool
+  bool b = true;
+  print(b);  
+}
