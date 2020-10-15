38540a4f7cb5f3985b04a5e046c1bec8e5b0d476
Shiftgithub <60180521+Shiftgithub@users.noreply.github.com>
2020-10-15 21:57:50 +0600

    ForInLoop

---
+++ Basic/D_ForInLoop.dart
@@ -0,0 +1,15 @@
+/**
+ * Syntax Of For in Loop
+ *
+ * for (variablename in object){  
+ *  statement or block to execute  
+ * }
+ */
+
+void main() { 
+   var obj = [12,13,14]; 
+   
+   for (var prop in obj) { 
+      print(prop); 
+   } 
+} 
