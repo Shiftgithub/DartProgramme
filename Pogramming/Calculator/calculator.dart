cacda50d40c5edd820bfa41c63f03ef1ec599afc
sajibAdhi <sajibadhikery@gmail.com>
2021-01-08 13:34:23 +0600

    Calculator

--- Pogramming/Calculator/calculator.dart
+++ Pogramming/Calculator/calculator.dart
@@ -11,3 +11,28 @@ void main() {
 
   print("Hello, ${num1 + num2} \nWelcome to GeeksforGeeks!!");
 }
+
+class Calculator {
+  var num1, num2;
+
+  int add(num1, num2){
+    int result = int.parse(num1) + int.parse(num2);
+    return result;
+  }
+
+  int subtract(num1, num2){
+    int result = int.parse(num1) - int.parse(num2);
+    return result;
+  }
+
+  int multiply(num1, num2){
+    int result = int.parse(num1) * int.parse(num2);
+    return result;
+  }
+
+  dynamic divide(num1, num2){
+    var result = int.parse(num1) / int.parse(num2);
+    return result;
+  }
+
+}
