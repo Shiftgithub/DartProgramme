690c7314789151ed16fd353147dcb8a5db55c1dd
sajibAdhi <60180521+Shiftgithub@users.noreply.github.com>
2021-03-19 10:59:51 +0600

    symbol

--- Basic/C_ForLoopMultipleAssingment.dart
+++
@@ -1,5 +0,0 @@
-void main() { 
-   for(var temp, i = 0, j = 1; j<30; temp = i, i = j, j = i + temp) { 
-      print('${j}'); 
-   } 
-} 
--- Pogramming/Calculator/calculator.dart
+++ Pogramming/Calculator/calculator.dart
@@ -5,8 +5,8 @@ void main() {
 
   print("Two Numbers: ");
 
-  var num1 = int.parse(scan());
-  var num2 = int.parse(scan());
+  int num1 = int.parse(scan()!);
+  int num2 = int.parse(scan()!);
   // var result = ;
 
   print("Hello, ${num1 + num2} \nWelcome to GeeksforGeeks!!");
